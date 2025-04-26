d8341cdec1b2552579405e05043990a4dcaccf08
wickedpotato69 <amrevhsuya@gmail.com>
2025-04-27 01:36:23 +0530

    cherry picked it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,4 +14,10 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+//just a random commit to test github actions, this is with correction 1
+// this is correction 2
+
+
 reportWebVitals();
